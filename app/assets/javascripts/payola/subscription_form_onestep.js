eff31edcff2d8b39fadf831b393e57bf7365cf55
Manuel J <manuel.perez.rubio@gmail.com>
2015-09-28 18:19:03 +0200

    i18n-capable one-step form and error handling improvements

--- app/assets/javascripts/payola/subscription_form_onestep.js
+++ app/assets/javascripts/payola/subscription_form_onestep.js
@@ -1,22 +1,68 @@
 var PayolaOnestepSubscriptionForm = {
+    timeout_message: "This seems to be taking too long. Please contact support and give them transaction ID: ",
+    connection_error: "There was a problem connecting to the server. Please, try again.",
+    errorMessages: {
+      incorrect_number: "The card number is incorrect.",
+      invalid_number: "The card number is not a valid credit card number.",
+      invalid_expiry_month: "The card's expiration month is invalid.",
+      invalid_expiry_year: "The card's expiration year is invalid.",
+      invalid_cvc: "The card's security code is invalid.",
+      expired_card: "The card has expired.",
+      incorrect_cvc: "The card's security code is incorrect.",
+      incorrect_zip: "The card's zip code failed validation.",
+      card_declined: "The card was declined.",
+      missing: "There is no card on a customer that is being charged.",
+      processing_error: "An error occurred while processing the card.",
+      rate_limit:  "An error occurred due to requests hitting the API too quickly. Please let us know if you're consistently running into this error."
+    },
+
     initialize: function() {
+        this.submitErrorHandlingSetup();
         $(document).on('submit', '.payola-onestep-subscription-form', function() {
             return PayolaOnestepSubscriptionForm.handleSubmit($(this));
         });
     },
 
+    submitErrorHandlingSetup: function() {
+        $(':submit').click(function() {
+            var error_selector = $('form').data('payola-error-selector');
+            if (error_selector) {
+                $(error_selector).text('');
+                $(error_selector).hide();
+            } else {
+                $('form').find('.payola-payment-error').text('');
+                $('form').find('.payola-payment-error').hide();
+            }
+        });
+    },
+
     handleSubmit: function(form) {
-        $(':submit').prop('disabled', true);
-        $('.payola-spinner').show();
+        this.disable_interactions();
+
         Stripe.card.createToken(form, function(status, response) {
             PayolaOnestepSubscriptionForm.stripeResponseHandler(form, status, response);
         });
         return false;
     },
 
+
+    disable_interactions: function() {
+        $(':submit').prop('disabled', true);
+        $('input[type="text"]').prop('disabled', true);
+        $('input[type="text"]').css('cursor', 'not-allowed');
+        $('.payola-spinner').show();
+    },
+
+    enable_interactions: function() {
+        $(':submit').prop('disabled', false);
+        $('input[type="text"]').prop('disabled', false);
+        $('input[type="text"]').css('cursor', 'default');
+        $('.payola-spinner').hide();
+    },
+
     stripeResponseHandler: function(form, status, response) {
         if (response.error) {
-            PayolaOnestepSubscriptionForm.showError(form, response.error.message);
+            PayolaOnestepSubscriptionForm.showError(form, response.error.message, response.error.code);
         } else {
             var email = form.find("[data-payola='email']").val();
             var coupon = form.find("[data-payola='coupon']").val();
@@ -40,14 +86,22 @@ var PayolaOnestepSubscriptionForm = {
                 url: action,
                 data: form.serialize(),
                 success: function(data) { PayolaOnestepSubscriptionForm.poll(form, 60, data.guid, base_path); },
-                error: function(data) { PayolaOnestepSubscriptionForm.showError(form, data.responseJSON.error); }
+                error: function(data) {
+                    var error_msg = '';
+                    if(data && data.responseJSON && data.responseJSON.error) {
+                        error_msg = data.responseJSON.error;
+                    } else {
+                        error_msg = PayolaOnestepSubscriptionForm.connection_error;
+                    }
+                    PayolaOnestepSubscriptionForm.showError(form, error_msg);
+                }
             });
         }
     },
 
     poll: function(form, num_retries_left, guid, base_path) {
         if (num_retries_left === 0) {
-            PayolaOnestepSubscriptionForm.showError(form, "This seems to be taking too long. Please contact support and give them transaction ID: " + guid);
+            PayolaOnestepSubscriptionForm.showError(form, this.timeout_message + guid);
         }
         var handler = function(data) {
             if (data.status === "active") {
@@ -57,7 +111,9 @@ var PayolaOnestepSubscriptionForm = {
             }
         };
         var errorHandler = function(jqXHR){
-            PayolaOnestepSubscriptionForm.showError(form, jqXHR.responseJSON.error);
+          if(jqXHR.responseJSON.status === "errored"){
+            PayolaSubscriptionForm.showError(form, jqXHR.responseJSON.error);
+          }
         };
 
         $.ajax({
@@ -69,9 +125,12 @@ var PayolaOnestepSubscriptionForm = {
         });
     },
 
-    showError: function(form, message) {
-        $('.payola-spinner').hide();
-        $(':submit').prop('disabled', false);
+    showError: function(form, message, code) {
+        this.enable_interactions();
+
+        if(code) {
+            var message = PayolaOnestepSubscriptionForm.errorMessages[code];
+        }
         var error_selector = form.data('payola-error-selector');
         if (error_selector) {
             $(error_selector).text(message);
